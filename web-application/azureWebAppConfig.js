d1fef7a2075e01bae589e5315ea13f948579d248
arcsoftech <arihant.chhajed@tcs.com>
2018-09-26 10:17:36 +0530

    Fixed incorrect environment name

--- web-application/azureWebAppConfig.js
+++ web-application/azureWebAppConfig.js
@@ -5,7 +5,7 @@ module.exports = () => {
     process.env.MY_DB_USER = process.env.MYSQLCONNSTR_USER;
     process.env.MY_DB_PASS = process.env.MYSQLCONNSTR_PASS;
     process.env.MY_DB = process.env.MYSQLCONNSTR_DB;
-    process.env.MS_API_ENDPOINT = process.env.APPLICATIONSETTING_MS_API_ENDPOINT;
-    process.env.MS_SUBSCRIPTION_KEY = process.env.APPLICATIONSETTING_MS_SUBSCRIPTION_KEY;
-    process.env.ENVIRONMENT = process.env.APPLICATIONSETTING_ENV;
+    process.env.MS_API_ENDPOINT = process.env.APPSETTING_MS_API_ENDPOINT;
+    process.env.MS_SUBSCRIPTION_KEY = process.env.APPSETTING_MS_SUBSCRIPTION_KEY;
+    process.env.ENVIRONMENT = process.env.APPSETTING_ENV;
 }
